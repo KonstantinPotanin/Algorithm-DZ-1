6ea1dd442e5f2d5dd16c2889b1c58156cb31669d
KonstantinPotanin <pkv90@yandex.ru>
2023-08-01 19:13:58 +0500

    123

---
+++ __init__.py
@@ -0,0 +1,33 @@
+def heap(arr, n, i):
+    largest = i
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2
+
+    if left_child < n and arr[i] < arr[left_child]:
+        largest = left_child
+
+    if right_child < n and arr[largest] < arr[right_child]:
+        largest = right_child
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heap(arr, n, largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+
+    # Build a max heap
+    for i in range(n // 2 - 1, -1, -1):
+        heap(arr, n, i)
+
+    # Extract elements one by one
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heap(arr, i, 0)
+
+
+unsorted_array = [12, 11, 13, 5, 6, 7]
+print(unsorted_array)
+heap_sort(unsorted_array)
+print(unsorted_array)
